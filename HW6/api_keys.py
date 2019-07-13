77aa46217bddcadddaa9fef72ced3869e7ab224b
MarcoASB <50678409+MarcoASB@users.noreply.github.com>
2019-07-12 19:16:07 -0500

    Add files via upload

---
+++ HW6/api_keys.py
@@ -0,0 +1,3 @@
+#w_key = "3a055dd6a7f958087c8ba0131763ad2f"
+#api_key = "O8f3ElfQGQ99PoAF" #This API key is for articles in NYT
+api_key= "3a055dd6a7f958087c8ba0131763ad2f" #This API key is for the weather forecast
